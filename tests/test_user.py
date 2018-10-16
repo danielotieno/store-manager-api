d400966764dfb738dc513c731df0e0eab8206e63
Daniel Otieno <danielotieno.ke@gmail.com>
2018-10-16 14:32:11 +0300

    [Chore #161210504] Add test for user cannot regester twice with same details

--- tests/test_user.py
+++ tests/test_user.py
@@ -28,3 +28,13 @@ class UserTests(BaseClass):
         self.assertEqual(response.status_code, 400)
         result = json.loads(response.data.decode())
         self.assertEqual(result["message"], "All fields are required")
+
+    def test_user_cannot_register_twice(self):
+        """ Test User cannot register twice """
+        self.client.post(SIGNUP_URL,
+                         data=json.dumps(self.user_data), content_type='application/json')
+        response2 = self.client.post(SIGNUP_URL,
+                                     data=json.dumps(self.user_data), content_type='application/json')
+        self.assertEqual(response2.status_code, 203)
+        result = json.loads(response2.data.decode())
+        self.assertEqual(result["message"], "User already exists")
