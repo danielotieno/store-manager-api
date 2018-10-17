4463a1dd99fa2a81999054652a211006680d74f4
Daniel Otieno <danielotieno.ke@gmail.com>
2018-10-17 22:33:32 +0300

    [Chore #161202934] Add test for product creation

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,10 +1,8 @@
-import os
 import unittest
 import json
 
 
 from app.v1 import create_app
-
 
 ADD_ENTRY_URL = '/api/v1/products/7'
 ADD_UPDATE_URL = '/api/v1/products/8'
@@ -14,95 +12,36 @@ DELETE_URL = '/api/v1/products/2'
 MODIFY_URL = '/api/v1/products/8'
 
 
-class TestProducts(unittest.TestCase):
-    """ Class for setup tests """
+class TestProduct(unittest.TestCase):
+    """This is the product class for test cases."""
 
-    def create_app(self):
-        """ Add Test configuration """
+    def setUp(self):
+        """Initialize app and define test variables"""
         self.app = create_app("testing")
         self.client = self.app.test_client()
 
         self.create_product = json.dumps(dict(
-            name="Shirt",
-            description="Modern polo shirt",
-            price=350,
-            category="Polo",
-            quantity=3,
+            name='Shirt',
+            description='Cool Polo shirt',
+            price=500,
+            category='Polo',
+            quantity=5,
             low_inventory=10))
 
-    def test_add_a_product(self):
-        """ Test to add a product """
+        self.client.post(
+            GET_ALL_URL,
+            data=self.create_product,
+            content_type='application/json')
+
+    def test_add_product(self):
+        """ Test for product creation """
+
         resource = self.client.post(
             GET_ALL_URL,
             data=self.create_product,
             content_type='application/json')
 
-        data = json.loads(resource.data.decode('utf-8'))
+        data = json.loads(resource.data.decode())
         print(data)
         self.assertEqual(resource.status_code, 201)
         self.assertEqual(resource.content_type, 'application/json')
-
-    def test_get_all_products(self):
-        """ Test to get all products """
-
-        response = self.client.get(
-            GET_ALL_URL, content_type='application/json')
-
-        data = json.loads(response.data.decode('utf-8'))
-        print(data)
-        self.assertEqual(response.content_type, 'application/json')
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_specific_product(self):
-        """ Test to fetch a specific product by id """
-
-        response = self.client.get(
-            GET_SINGLE_URL, data=json.dumps(dict(name="Shirt",
-                                                 description="Modern polo shirt",
-                                                 price=350,
-                                                 category="Polo",
-                                                 low_inventory=10
-                                                 )), content_type='application/json')
-        result = json.loads(response.data.decode('utf-8'))
-        print(result)
-        self.assertEqual(response.status_code, 200)
-
-    def test_update_a_product(self):
-        """ Test to update a product """
-        response = self.client.post(GET_ALL_URL, data=self.create_product,
-                                    content_type='application/json')
-
-        data = json.loads(response.data.decode('utf-8'))
-        print(data)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.content_type, 'application/json')
-
-        response = self.client.put(MODIFY_URL,
-                                   data=json.dumps(dict(
-                                       name="Shirt",
-                                       description="Modern polo shirt",
-                                       price=500,
-                                       category="Gucci",
-                                       low_inventory=10
-
-
-                                   )), content_type=("application/json"))
-        self.assertEqual(response.status_code, 200)
-        result = json.loads(response.data.decode())
-        print(result)
-
-    def test_delete_an_order(self):
-        """ Test to delete a product """
-        response = self.client.delete(
-            DELETE_URL, data=json.dumps(dict(product_id=2,
-                                             name="Jeans",
-                                             description="Brand new khaki jeans",
-                                             price=500,
-                                             category="Khaki",
-                                             low_inventory=10
-                                             )), content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-
-
-if __name__ == '__main__':
-    unittest.main()
