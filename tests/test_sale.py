724c502ca713f0ef94f97e3418e93ce1d0cbdb7a
Daniel Otieno <danielotieno.ke@gmail.com>
2018-10-23 00:49:43 +0300

    [Chore #161336932] Add test for getting all sales record

--- tests/test_sale.py
+++ tests/test_sale.py
@@ -31,11 +31,12 @@ class TestSale(unittest.TestCase):
             total_amount=3000
         ))
 
-        self.user_data = {
-            "username": "danny",
-            "email": "danny@gmail.com",
-            "password": "password1",
-            "role": "Store Attendant"
+        self.admin_data = {
+            "username": "admin",
+            "email": "admin@email.com",
+            "password": "admin12345",
+            "role": "Admin"
+
         }
 
         self.client.post(
@@ -46,12 +47,12 @@ class TestSale(unittest.TestCase):
     def logged_in_user(self):
         # first create user
         self.client.post(SIGNUP_URL,
-                         data=json.dumps(self.user_data), content_type='application/json')
+                         data=json.dumps(self.admin_data), content_type='application/json')
 
         # then log in user
         res = self.client.post(LOGIN_URL,
                                data=json.dumps(
-                                   self.user_data),
+                                   self.admin_data),
                                content_type='application/json')
 
         result = json.loads(res.data.decode('utf-8'))
@@ -64,32 +65,36 @@ class TestSale(unittest.TestCase):
 
         return token
 
-    def test_add_sale(self):
-        """ Test for sale order creation """
-        access_token = self.get_token()
+    # def test_add_sale(self):
+    #     """ Test for sale order creation """
+    #     access_token = self.get_token()
+    #     print(access_token)
 
-        resource = self.client.post(
+    #     resource = self.client.post(
+    #         GET_ALL_URL,
+    #         data=self.create_sale,
+    #         content_type='application/json',
+    #         headers={'Authorization': 'Bearer '+access_token})
+
+    #     data = json.loads(resource.data.decode('utf-8'))
+    #     print(data)
+    #     self.assertEqual(resource.status_code, 201)
+    #     self.assertEqual(resource.content_type, 'application/json')
+
+    def test_get_all_sales(self):
+        """ Test for getting all sales record """
+        access_token = self.get_token()
+        print(access_token)
+        resource = self.client.get(
             GET_ALL_URL,
-            data=self.create_sale,
+            data=json.dumps(dict()),
             content_type='application/json',
             headers={'Authorization': 'Bearer '+access_token})
 
-        data = json.loads(resource.data.decode('utf-8'))
+        data = json.loads(resource.data.decode())
         print(data)
-        self.assertEqual(resource.status_code, 201)
+        self.assertEqual(resource.status_code, 200)
         self.assertEqual(resource.content_type, 'application/json')
-
-    # def test_get_all_sales(self):
-    #     """ Test for getting all sales record """
-    #     resource = self.client.get(
-    #         GET_ALL_URL,
-    #         data=json.dumps(dict()),
-    #         content_type='application/json')
-
-    #     data = json.loads(resource.data.decode())
-    #     print(data)
-    #     self.assertEqual(resource.status_code, 200)
-    #     self.assertEqual(resource.content_type, 'application/json')
 
     def test_get_specific_sale_by_id(self):
         """ Test for getting specific sale record by id """
--- tests/test_user.py
+++ tests/test_user.py
@@ -10,26 +10,17 @@ LOGIN_URL = '/api/v1/auth/login'
 class UserTests(BaseClass):
     """ Defining and setup user class tests """
 
-    # def test_admin_registration(self):
-    #     """ Test user registration works correcty """
-    #     access_token = self.get_token()
-    #     response = self.client.post(SIGNUP_URL,
-    #                                 data=json.dumps(self.admin_data),
-    #                                 headers={'content_type': 'application/json',
-    #                                          'Authorization': 'Bearer '+access_token})
-    #     self.assertEqual(response.status_code, 201)
-    #     result = json.loads(response.data.decode())
-    #     self.assertEqual(result["message"],
-    #                      "registration successful, now login")
-
-    # def test_user_registration(self):
-    #     """ Test user registration works correcty """
-    #     response = self.client.post(SIGNUP_URL,
-    #                                 data=json.dumps(self.user_data), content_type='application/json')
-    #     self.assertEqual(response.status_code, 201)
-    #     result = json.loads(response.data.decode())
-    #     self.assertEqual(result["message"],
-    #                      "registration successful, now login")
+    def test_user_registration(self):
+        """ Test user registration works correcty """
+        access_token = self.get_token()
+        print(access_token)
+        response = self.client.post(SIGNUP_URL,
+                                    data=json.dumps(self.user_data), content_type='application/json',
+                                    headers={'Authorization': 'Bearer '+access_token})
+        self.assertEqual(response.status_code, 201)
+        result = json.loads(response.data.decode())
+        self.assertEqual(result["message"],
+                         "registration successful, now login")
 
     # def test_user_wrong_registration(self):
     #     """Test wrong registration when user doesn't fill fields"""
