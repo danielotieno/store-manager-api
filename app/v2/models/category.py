7d2bc2e9f692120704e4ef9845dd4613afb38e0f
Daniel Otieno <danielotieno.ke@gmail.com>
2018-10-24 16:04:37 +0300

    [Chore #161409532] Create a method to add categories

---
+++ app/v2/models/category.py
@@ -0,0 +1,30 @@
+"""
+This model defines a category class and it's methods
+It also create data structure to store category data
+
+"""
+import uuid
+from datetime import datetime
+from flask import request
+from flask import current_app
+
+
+class Category:
+    """ Create a Category class to hold cetegories methods """
+
+    def __init__(self):
+        """ Initialize empty Category list"""
+        self.category_list = []
+
+    def create_cetegory(self, name, status):
+        """ A method to create categories """
+
+        self.category_details = {}
+
+        self.category_details['product_id'] = str(uuid.uuid1())
+        self.category_details['name'] = name
+        self.category_details['status'] = status
+        self.category_details['date'] = str(datetime.now().replace(
+            second=0, microsecond=0))
+        self.category_list.append(self.category_details)
+        return {'Categories': self.category_list, 'message': 'Category added successfully'}, 201
