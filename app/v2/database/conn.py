55168ca5c9750ae1c1ee339cfa762d5bb1edf9b7
Daniel Otieno <danielotieno.ke@gmail.com>
2018-10-25 02:05:08 +0300

    [Chore #161408308] Initialize a database

--- app/v2/database/conn.py
+++ app/v2/database/conn.py
@@ -7,11 +7,31 @@ import psycopg2
 
 from app.v2.database.tables import table_list, tables_to_drop
 
+
 def database_connection():
     """ create a database connection  """
 
     url = os.getenv('DATABASE_URL')
     return psycopg2.connect(url)
+
+
+def init_db():
+    """Initialize a database"""
+    try:
+        connection = dbcon()
+        connection.autocommit = True
+
+        # activate cursor
+        cursor = connection.cursor()
+
+        for table in table_list:
+            cursor.execute(query)
+        connection.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("DB Error")
+        print(error)
+
 
 def drop_all_tables():
     """A method to drop tables """
@@ -21,5 +41,3 @@ def drop_all_tables():
         cursor.execute(drop)
         connection.commit()
     connection.close()
-
-
